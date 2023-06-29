fe6743480bc02ba38fc5f6dd3dbd7536e09c41f0
2º DSM <dsm-2@fatec.academico>
2023-06-29 19:17:36 -0300

    loader funcionando

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Loader from '../Loader/Loader.jsx';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
 import { faBurst } from '@fortawesome/free-solid-svg-icons';
@@ -15,13 +17,19 @@ const Card = () => {
     const [name, setName] = React.useState('bulbasaur');
     const [search, setSearch] = React.useState(name);
     const [pokemon, setPokemon] = React.useState();
+    const [loading, setLoading] = React.useState(false);
+
 
     React.useEffect(() => {
         async function searchPokemon(){
-            try{
+            try{          
+                setLoading(true);
+
                 const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
                 const json = await response.json();
+
                 setPokemon(json);
+                setLoading(false);
             }
             catch{
                 console.log('Erro na requisão à API.');
@@ -29,12 +37,17 @@ const Card = () => {
         }
         searchPokemon();
     }, [name]);
+
+    function handleClick(){
+        setLoading(true);
+        setName(search);
+    }
     
     if(pokemon == undefined){
         return (
             <div className="boxSearch">
                     <input type="text" placeholder='Pesquisar Pokémon' value={search} onChange={({target}) => setSearch(target.value)}/>
-                    <button onClick={() => setName(search)}>Pesquisar</button>
+                    <button onClick={handleClick}>Pesquisar</button>
             </div>
         )
     }
@@ -43,46 +56,51 @@ const Card = () => {
         <>
             <div className="boxSearch">
                 <input type="text" placeholder='Pesquisar Pokémon' value={search} onChange={({target}) => setSearch(target.value)}/>
-                <button onClick={() => setName(search)}>Pesquisar</button>
+                <button onClick={handleClick}>Pesquisar</button>
             </div>
 
             <section className='containerCard'>
-                <header>
-                    <span className='spanIdPokemon'>{pokemon.id}</span>
-                    <p className='namePokemon'>{pokemon.name}</p>
-                    <p className='typePokemon'>{pokemon.types[0].type.name}</p>
-                </header>
-                <main>
-                    <img src={pokemon.sprites.front_default} className='imgPokemon'/>
-                </main>
-                <footer>
-                    <div className="boxStats">
-                        <label className='lblHp'>
-                            <p>
-                                <FontAwesomeIcon icon={faHeart} className='iconFontAwesome'/>
-                                <span>{pokemon.stats[0].base_stat}</span>
-                            </p>
-                        </label>
-                        <label className='lblAtk'>
-                            <p>
-                                <FontAwesomeIcon icon={faBurst} className='iconFontAwesome'/>
-                                <span>{pokemon.stats[1].base_stat}</span>
-                            </p>
-                        </label>
-                        <label className='lblDef'>
-                            <p>
-                                <FontAwesomeIcon icon={faShield} className='iconFontAwesome'/>
-                                <span>{pokemon.stats[2].base_stat}</span>
-                            </p>
-                        </label>
-                        <label className='lblWeight'>
-                            <p>
-                                <FontAwesomeIcon icon={faWeightScale} className='iconFontAwesome'/>
-                                <span>{pokemon.stats[3].base_stat}</span>
-                            </p>
-                        </label>
-                    </div>
-                </footer>
+                {loading && <Loader />}
+                {!loading  && (
+                    <>
+                        <header>
+                            <span className='spanIdPokemon'>{pokemon.id}</span>
+                            <p className='namePokemon'>{pokemon.name}</p>
+                            <p className='typePokemon'>{pokemon.types[0].type.name}</p>
+                        </header>
+                        <main>
+                            <img src={pokemon.sprites.front_default} className='imgPokemon'/>
+                        </main>
+                        <footer>
+                            <div className="boxStats">
+                                <label className='lblHp'>
+                                    <p>
+                                        <FontAwesomeIcon icon={faHeart} className='iconFontAwesome'/>
+                                        <span>{pokemon.stats[0].base_stat}</span>
+                                    </p>
+                                </label>
+                                <label className='lblAtk'>
+                                    <p>
+                                        <FontAwesomeIcon icon={faBurst} className='iconFontAwesome'/>
+                                        <span>{pokemon.stats[1].base_stat}</span>
+                                    </p>
+                                </label>
+                                <label className='lblDef'>
+                                    <p>
+                                        <FontAwesomeIcon icon={faShield} className='iconFontAwesome'/>
+                                        <span>{pokemon.stats[2].base_stat}</span>
+                                    </p>
+                                </label>
+                                <label className='lblWeight'>
+                                    <p>
+                                        <FontAwesomeIcon icon={faWeightScale} className='iconFontAwesome'/>
+                                        <span>{pokemon.stats[3].base_stat}</span>
+                                    </p>
+                                </label>
+                            </div>
+                        </footer>
+                    </>
+                )}
             </section>
         </>
     )
