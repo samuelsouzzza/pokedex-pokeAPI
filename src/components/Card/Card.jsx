784477d7309a5271557fdb96b51773908b473931
2º DSM <dsm-2@fatec.academico>
2023-06-28 21:07:41 -0300

    barra de pesquisa funcionando mas da erro quando pesquisa vazio

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -12,76 +12,80 @@ import './Card.css';
 
 const Card = () => {
 
-    const [search, setSearch] = React.useState();
+    const [name, setName] = React.useState('bulbasaur');
+    const [search, setSearch] = React.useState(name);
     const [pokemon, setPokemon] = React.useState();
 
-    if(pokemon == null || search == null){
-        React.useEffect(() => {
-            fetch(`https://pokeapi.co/api/v2/pokemon/${search}`)
-            .then(response => response.json())
-            .then(data => setPokemon(data));
-        
-        }, [search, pokemon]);
+    React.useEffect(() => {
+        async function searchPokemon(){
+            try{
+                const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+                const json = await response.json();
+                setPokemon(json);
+            }
+            catch{
+                console.log('Erro na requisão à API.');
+            }
+        }
+        searchPokemon();
+    }, [name]);
+    
+    if(pokemon == undefined){
+        return (
+            <div className="boxSearch">
+                    <input type="text" placeholder='Pesquisar Pokémon' value={search} onChange={({target}) => setSearch(target.value)}/>
+                    <button onClick={() => setName(search)}>Pesquisar</button>
+            </div>
+        )
     }
 
+    return (
+        <>
+            <div className="boxSearch">
+                <input type="text" placeholder='Pesquisar Pokémon' value={search} onChange={({target}) => setSearch(target.value)}/>
+                <button onClick={() => setName(search)}>Pesquisar</button>
+            </div>
 
-        if(search == null || pokemon == null){
-            return (
-                 <div className="boxSearch">
-                    <input type="text" placeholder='Pesquisar Pokémon' value={search} onChange={({target}) => setSearch(target.value)}/>
-                </div>
-            )
-        }
-        return (
-            <>
-                <div className="boxSearch">
-                    <input type="text" placeholder='Pesquisar Pokémon' value={search} onChange={({target}) => setSearch(target.value)}/>
-                </div>
-                
-                <section className='containerCard'>
-                {pokemon ? (
-                    <>
-                        <header>
-                            <span className='spanIdPokemon'>{pokemon.id}</span>
-                            <p className='namePokemon'>{pokemon.name}</p>
-                            <p className='typePokemon'>{pokemon.types[0].type.name}</p>
-                        </header>
-                        <main>
-                            <img src={pokemon.sprites.front_default} className='imgPokemon'/>
-                        </main>
-                        <footer>
-                            <div className="boxStats">
-                                <label className='lblHp'>
-                                    <p>
-                                        <FontAwesomeIcon icon={faHeart} className='iconFontAwesome'/>
-                                        {pokemon.stats[0].base_stat}
-                                    </p>
-                                </label>
-                                <label className='lblAtk'>
-                                    <p>
-                                        <FontAwesomeIcon icon={faBurst} className='iconFontAwesome'/>
-                                        {pokemon.stats[1].base_stat}
-                                    </p>
-                                </label>
-                                <label className='lblDef'>
-                                    <p>
-                                        <FontAwesomeIcon icon={faShield} className='iconFontAwesome'/>
-                                        {pokemon.stats[2].base_stat}
-                                    </p>
-                                </label>
-                                <label className='lblWeight'>
-                                    <p>
-                                        <FontAwesomeIcon icon={faWeightScale} className='iconFontAwesome'/>
-                                        {Math.round(pokemon.weight / 100).toFixed(1) + 'kg'}
-                                    </p>
-                                </label>
-                            </div>
-                        </footer>
-                    </>
-                ) : <p>Carregando...</p> }
-                </section>
-            </>
-        )
+            <section className='containerCard'>
+                <header>
+                    <span className='spanIdPokemon'>{pokemon.id}</span>
+                    <p className='namePokemon'>{pokemon.name}</p>
+                    <p className='typePokemon'>{pokemon.types[0].type.name}</p>
+                </header>
+                <main>
+                    <img src={pokemon.sprites.front_default} className='imgPokemon'/>
+                </main>
+                <footer>
+                    <div className="boxStats">
+                        <label className='lblHp'>
+                            <p>
+                                <FontAwesomeIcon icon={faHeart} className='iconFontAwesome'/>
+                                <span>{pokemon.stats[0].base_stat}</span>
+                            </p>
+                        </label>
+                        <label className='lblAtk'>
+                            <p>
+                                <FontAwesomeIcon icon={faBurst} className='iconFontAwesome'/>
+                                <span>{pokemon.stats[1].base_stat}</span>
+                            </p>
+                        </label>
+                        <label className='lblDef'>
+                            <p>
+                                <FontAwesomeIcon icon={faShield} className='iconFontAwesome'/>
+                                <span>{pokemon.stats[2].base_stat}</span>
+                            </p>
+                        </label>
+                        <label className='lblWeight'>
+                            <p>
+                                <FontAwesomeIcon icon={faWeightScale} className='iconFontAwesome'/>
+                                <span>{pokemon.stats[3].base_stat}</span>
+                            </p>
+                        </label>
+                    </div>
+                </footer>
+            </section>
+        </>
+    )
 }
 
 export default Card;
