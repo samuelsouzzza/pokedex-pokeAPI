89d32093e8c212959bd2e8674f0734bffe8504d3
= <=>
2023-07-04 19:45:22 -0300

    ajustes nas cores e add a logo do pokemon

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,13 @@ import Card from './components/Card/Card.jsx';
 
 import './App.css';
 
+import logo from './assets/imgs/pokemon.svg';
+
 function App() {
 
   return (
     <div className='app'>
+      <img src={logo} className='logo' alt="Logo marca principal do Pokémon" />
       <Card />
     </div>
   )
